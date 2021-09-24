350c2156ae88bb1c989165c30b014ddf107b1e73
thewires2 <arnavbagchi04@gmail.com>
2021-09-24 23:42:24 +0530

    Update bootstrap changes and add images

--- Pradarshini/settings.py
+++ Pradarshini/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ng#y0dv)y#90jn^l4p=0_=8^q9lwf(of&5j%6wzg8536ow=hd6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','127.0.0.1']
 
 
 # Application definition
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Projects',
-    'crispy_forms'
+    'crispy_forms',
+    'Website',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
