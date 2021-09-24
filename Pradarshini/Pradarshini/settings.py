b3ddda2b0b6788af759aecd87a9a2625a5a72026
thewires2 <arnavbagchi04@gmail.com>
2021-09-24 13:57:24 +0530

    MIgrate from sqlite3 to postgres

--- Pradarshini/Pradarshini/settings.py
+++ Pradarshini/Pradarshini/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Pradarshini.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -126,10 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
