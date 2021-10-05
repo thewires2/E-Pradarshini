6714a263acf38462cf1001cfbf02ec63396b9ee1
Arnav Bagchi <arnavbagchi04@gmail.com>
2021-10-05 19:49:06 +0530

    Add main homepage

--- Pradarshini/urls.py
+++ Pradarshini/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("Projects.urls")),
+    path("", include("Website.urls")),
 ]
--- Website/urls.py
+++ Website/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.project_main, name="project_main"),
+    path("", views.home, name="home"),
 ]
